55756f23358acd8bfea9819ba8d54e8dc7a85288
coderbing1989 <notbin@qq.com>
2017-06-28 22:21:28 +0800

    第一次提交

---
+++ src/main/java/com/anju/AnjucustomerApplication.java
@@ -0,0 +1,13 @@
+package com.anju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnjucustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnjucustomerApplication.class, args);
+	}
+}
